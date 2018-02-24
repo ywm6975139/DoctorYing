e3d83728ab73f41768cadaa2fdd0b84e8fe5a9de
bxy <cangqiong1122@sina.cn>
2018-02-24 21:42:16 +0800

    工程结构调整

--- code/src/DealFile/FirstStep.py
+++ code/src/DealFile/FirstStep.py
@@ -1,24 +1,18 @@
 # -*- coding: utf-8 -*-
-import os
 import sys
-#import pandas as pd
-import json
-import time
-from Bio import SeqIO
-from Bio.Seq import Seq
-from Bio.SeqRecord import SeqRecord
-from Bio.Alphabet import generic_protein
 
+# import pandas as pd
+from Bio import SeqIO
 
 reload(sys)
 sys.setdefaultencoding('utf8')
 
 
-#转换文件格式从fastq转换成fasta
-def fileConvert(file_source_path,file_target_path):
+# 转换文件格式从fastq转换成fasta
+def fileConvert(file_source_path, file_target_path):
     records = SeqIO.parse(file_source_path, "fastq")
     count = SeqIO.write(records, file_target_path, "fasta")
-    if count>0:
+    if count > 0:
         print "file convert Success! Converted %i records" % count
     else:
         print "file convert Error!"
@@ -30,39 +24,32 @@ def fileConvert(file_source_path,file_target_path):
 # 如果重复片段大于等于30，返回 (2,'重复片段')
 def seqConcatDeal(seq1, seq2):
     if seq1 == seq2:
-        return 2,seq1
+        return 2, seq1
     else:
         seqCursor = 1
         while seqCursor < 150:
             if seq1[seqCursor:] == seq2[0:-seqCursor]:
                 if seqCursor <= 120:
-                    return 2,seq1[:seqCursor] + seq2
+                    return 2, seq1[:seqCursor] + seq2
                 else:
-                    return 1,seq1[:seqCursor] + seq2
+                    return 1, seq1[:seqCursor] + seq2
             else:
                 seqCursor = seqCursor + 1
-        return 0,None
-
-
-
-
+        return 0, None
 
 
 if __name__ == '__main__':
-    file_source_path1="../File/test1.fastq"
-    file_target_path1="../File/test1.fasta"
-    file_source_path2="../File/test2.fastq"
-    file_target_path2="../File/test2.fasta"
+    file_source_path1 = "../File/test1.fastq"
+    file_target_path1 = "../File/test1.fasta"
+    file_source_path2 = "../File/test2.fastq"
+    file_target_path2 = "../File/test2.fasta"
 
     # fileConvert(file_source_path1,file_target_path1)
     # fileConvert(file_source_path2,file_target_path2)
 
-
-    all_record=[]
+    all_record = []
     for seq_record in SeqIO.parse(file_target_path1, "fasta"):
         print str(seq_record.id)
         print str(seq_record.seq)
         print str(seq_record.seq.reverse_complement())
         print len(seq_record)
-
-
